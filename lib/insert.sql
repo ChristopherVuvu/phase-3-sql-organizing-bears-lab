3cbeb36ec2ed3722bfeb83e14df5c0670181b888
ChristopherVuvu <vuvumumo93@gmail.com>
2023-06-02 02:59:10 +0300

    added

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 28, 'M', 'White', 'Good', true);
+('Rowdy', 20, 'F', 'Black', 'Cool', true);
+('Tabitha', 23, 'F', 'Spotted', 'Fair', true);
+('Sergeant Brown', 21, 'M', 'Grey', 'Good', true);
+('Melissa', 19, 'F', 'Blackish-White', 'Tempered', true);
+('Grinch', 25, 'M', 'Reddish', 'Hot', true);
+('Wendy', 10, 'F', 'Brown', 'Cool', true);
+('NULL', NULL, NULL, NULL, NULL, NULL);
